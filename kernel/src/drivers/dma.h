161c45716e8413218fb8a80ceac05abc3e4b5867
strawberryhacker <bjorn.thoralf@outlook.com>
2020-07-13 23:36:23 +0200

    Added DMA files

---
+++ kernel/src/drivers/dma.h
@@ -0,0 +1,8 @@
+/// Copyright (C) StrawberryHacker
+
+#ifndef DMA_H
+#define DMA_H
+
+
+
+#endif
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -22,9 +22,9 @@ static void blink_thread(void* arg) {
 		//syscall_gpio_toggle(GPIOC, 8);
 		//syscall_thread_sleep(2);
 		//syscall_gpio_toggle(GPIOC, 8);
-		//syscall_thread_sleep(1000);
+		syscall_thread_sleep(1000);
 		//u64 runtime = cpu_rq.idle->runtime_curr;
-		//print("Runtime: %d\n", (u32)(runtime / 3000));
+		print("Runtime:\n");
 	}
 }
 
@@ -70,7 +70,7 @@ static void file(void* arg) {
 
 	// Try to open a file
 	struct file file;
-	fat_file_open(&file, "C:/App.bin", 10);
+	fat_file_open(&file, "C:/application.bin", 18);
 	
 	u32 write_status;
 	u8* file_buffer = (u8 *)mm_alloc_4k(1);
@@ -79,7 +79,7 @@ static void file(void* arg) {
 		fat_file_read(&file, file_buffer, 4000, &write_status);
 	} while (write_status == 4000);
 	printl("Application is loaded into memory");
-
+	print("App addr: %4h\n", file_buffer);
 	dynamic_linker_run((u32 *)file_buffer);
 	
 	while (1);
