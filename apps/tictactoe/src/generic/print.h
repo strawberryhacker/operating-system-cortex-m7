68f421164d26ccaaaeb07d4390978b399daf1874
strawberryhacker <bjorn.thoralf@outlook.com>
2020-07-20 00:38:12 +0200

    Added app submodules

--- apps/tictactoe/src/generic/print.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINT_H
-#define PRINT_H
-
-#include "types.h"
-#include <stdarg.h>
-
-void print(const char* data, ...);
-
-#endif
--- apps/tictactoe/src/main.c
+++ apps/tictactoe/src/main.c
@@ -1,4 +1,4 @@
-/// Copyright (C) StrawberryHacker
+/* Copyright (C) StrawberryHacker */
 
 #include "types.h"
 #include "syscall.h"
@@ -7,11 +7,6 @@
 
 int main(void) {
     
-    // ====================================================================
-    // Put the application code here. The function may return, the scheduler 
-    // will take care of it. No startup code is needed. Please read the 
-    // docmentation before implementing apps. 
-    // ====================================================================
     game_init();
     draw_board();
     game_loop();
