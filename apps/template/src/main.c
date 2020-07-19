9d27e404ac32dccc9fd4bb69c9925319c0f50f8d
strawberryhacker <bjorn.thoralf@outlook.com>
2020-07-20 00:46:19 +0200

    Added applocation template

---
+++ apps/template/src/main.c
@@ -0,0 +1,19 @@
+/* Copyright (C) StrawberryHacker */
+
+#include "types.h"
+#include "syscall.h"
+#include "hardware.h"
+#include "print.h"
+
+int main(void) {
+    
+    /* 
+     * ====================================================================
+     * Put your application code here. This function may return, the
+     * scheduler will take care of it. No startup code is needed.
+     * Please read the docmentation before implementing apps. 
+     * ====================================================================
+     */
+    
+    return 1;
+ }
